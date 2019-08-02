11a3452ff86a7cbd416a58f2f874294b117df0fb
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-03 06:40:51 +0700

    :pencil: setup viewmodel fragment notification

---
+++ app/src/main/java/com/dot/baseandroid/menu/notification/viewmodels/FragmentNotificationViewModel.kt
@@ -0,0 +1,7 @@
+package com.dot.baseandroid.menu.notification.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FragmentNotificationViewModel(application: Application) : AndroidViewModel(application) {
+}
--- app/src/main/java/com/dot/baseandroid/menu/notification/views/FragmentNotification.kt
+++ app/src/main/java/com/dot/baseandroid/menu/notification/views/FragmentNotification.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.dot.baseandroid.R
+import com.dot.baseandroid.databinding.FragmentNotificationBinding
+import com.dot.baseandroid.menu.notification.viewmodels.FragmentNotificationViewModel
 
 class FragmentNotification: Fragment() {
 
+    private lateinit var binding: FragmentNotificationBinding
+    private lateinit var viewModel: FragmentNotificationViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notification, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = FragmentNotificationViewModel(activity?.application!!)
+        binding.notification = viewModel
     }
 
 }

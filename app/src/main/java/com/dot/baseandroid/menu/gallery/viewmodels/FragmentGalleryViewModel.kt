720a6cdc3845120f3912500cf8057b3b6e335d2b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-02 16:56:16 +0700

    :pencil: setup viewmodel fragment gallery

---
+++ app/src/main/java/com/dot/baseandroid/menu/gallery/viewmodels/FragmentGalleryViewModel.kt
@@ -0,0 +1,7 @@
+package com.dot.baseandroid.menu.gallery.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FragmentGalleryViewModel(application: Application) : AndroidViewModel(application) {
+}
--- app/src/main/java/com/dot/baseandroid/menu/gallery/views/FragmentGallery.kt
+++ app/src/main/java/com/dot/baseandroid/menu/gallery/views/FragmentGallery.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.dot.baseandroid.R
+import com.dot.baseandroid.databinding.FragmentGalleryBinding
+import com.dot.baseandroid.menu.gallery.viewmodels.FragmentGalleryViewModel
 
 class FragmentGallery: Fragment() {
 
+    private lateinit var binding: FragmentGalleryBinding
+    private lateinit var viewModel: FragmentGalleryViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gallery, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = FragmentGalleryViewModel(activity?.application!!)
+        binding.gallery = viewModel
     }
 
 }

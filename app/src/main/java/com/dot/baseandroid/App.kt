eb4f6a3f5080ef426feaaf1780003e3affc926b1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-02 15:56:44 +0700

    :pencil: add kotlin extension

--- app/src/main/java/com/dot/baseandroid/App.kt
+++ app/src/main/java/com/dot/baseandroid/App.kt
@@ -1,10 +1,12 @@
 package com.dot.baseandroid
 
 import android.app.Application
+import com.dot.baseandroid.utils.logDebug
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        logDebug("App # onCreate")
     }
 }
--- app/src/main/java/com/dot/baseandroid/utils/AppExtension.kt
+++ app/src/main/java/com/dot/baseandroid/utils/AppExtension.kt
@@ -1,1 +1,13 @@
 package com.dot.baseandroid.utils
+
+import android.util.Log
+import com.dot.baseandroid.BuildConfig
+import com.dot.baseandroid.data.Constants
+
+fun logDebug(message: String) {
+    if (BuildConfig.DEBUG) Log.d(Constants.TAG_DEBUG, message)
+}
+
+fun logError(message: String) {
+    if (BuildConfig.DEBUG) Log.e(Constants.TAG_ERROR, message)
+}

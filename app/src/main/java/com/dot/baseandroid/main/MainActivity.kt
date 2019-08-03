b4e8ff3c0feca82549ebeb5ef9d0273f629257c6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-03 07:41:59 +0700

    :pencil: setup toolbar font

--- app/src/main/java/com/dot/baseandroid/main/MainActivity.kt
+++ app/src/main/java/com/dot/baseandroid/main/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbarMain)
-        //supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     private fun setupNavigation() {

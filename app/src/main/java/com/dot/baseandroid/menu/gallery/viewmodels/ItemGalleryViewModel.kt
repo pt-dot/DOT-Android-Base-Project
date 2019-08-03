ae940eb189c1f017d9a09934a83998e87ba12d01
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-03 18:03:18 +0700

    :pencil: add layout item gallery

---
+++ app/src/main/java/com/dot/baseandroid/menu/gallery/viewmodels/ItemGalleryViewModel.kt
@@ -0,0 +1,7 @@
+package com.dot.baseandroid.menu.gallery.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.dot.baseandroid.menu.gallery.models.GalleryModel
+
+class ItemGalleryViewModel(galleryModel: GalleryModel): ViewModel() {
+}

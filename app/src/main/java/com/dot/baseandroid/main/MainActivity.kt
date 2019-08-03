228396b79ebd63920031afbbdc57d06ad2a57f4c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-03 07:25:06 +0700

    :construction: add navigation

--- app/src/main/java/com/dot/baseandroid/main/MainActivity.kt
+++ app/src/main/java/com/dot/baseandroid/main/MainActivity.kt
@@ -2,7 +2,11 @@ package com.dot.baseandroid.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.dot.baseandroid.R
 import com.dot.baseandroid.databinding.ActivityMainBinding
 
@@ -12,6 +16,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupToolbar()
+        setupNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.fragmentContainer).navigateUp()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbarMain)
+        //supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setupNavigation() {
+        binding.bottomNavigationViewMain.itemIconTintList = null
+        val navController = Navigation.findNavController(this, R.id.fragmentContainer)
+        setupActionBarWithNavController(navController)
+        NavigationUI.setupWithNavController(binding.bottomNavigationViewMain, navController)
     }
 }

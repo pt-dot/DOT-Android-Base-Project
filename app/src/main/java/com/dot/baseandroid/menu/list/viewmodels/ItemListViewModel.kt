0f6e4de4568c43c0247a67259c405c3763308d1e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-04 17:03:37 +0700

    :construction: add item list layout

---
+++ app/src/main/java/com/dot/baseandroid/menu/list/viewmodels/ItemListViewModel.kt
@@ -0,0 +1,7 @@
+package com.dot.baseandroid.menu.list.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.dot.baseandroid.menu.list.models.PlaceModel
+
+class ItemListViewModel(placeModel: PlaceModel): ViewModel() {
+}

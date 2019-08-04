b6e1dc18f3ec36bc8d35a68368483a5a7eab3b78
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-04 10:20:22 +0700

    :sparkles: show list gallery

--- app/src/main/java/com/dot/baseandroid/menu/gallery/views/FragmentGallery.kt
+++ app/src/main/java/com/dot/baseandroid/menu/gallery/views/FragmentGallery.kt
@@ -8,14 +8,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dot.baseandroid.R
 import com.dot.baseandroid.databinding.FragmentGalleryBinding
+import com.dot.baseandroid.menu.gallery.adapters.GalleryAdapter
 import com.dot.baseandroid.menu.gallery.viewmodels.FragmentGalleryViewModel
 
 class FragmentGallery: Fragment() {
 
     private lateinit var binding: FragmentGalleryBinding
     private lateinit var viewModel: FragmentGalleryViewModel
+
+    private lateinit var adapter: GalleryAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gallery, container, false)
@@ -27,6 +31,8 @@ class FragmentGallery: Fragment() {
         viewModel = ViewModelProviders.of(this).get(FragmentGalleryViewModel::class.java)
         binding.gallery = viewModel
 
+        setupSwipeRefresh()
+        setupRecyclerView()
         observeData()
 
         viewModel.getListGallery(context!!)
@@ -34,11 +40,25 @@ class FragmentGallery: Fragment() {
 
     private fun observeData() {
         viewModel.isLoading.observe(this, Observer {
-
+            binding.swipeRefreshListGallery.isRefreshing = it
         })
         viewModel.liveDataListGallery.observe(this, Observer {
-
+            adapter.submitList(it)
         })
+    }
+
+    private fun setupSwipeRefresh() {
+        binding.swipeRefreshListGallery.setOnRefreshListener {
+            viewModel.getListGallery(context!!)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerViewListGallery.layoutManager = GridLayoutManager(context, 3)
+        adapter = GalleryAdapter {
+
+        }
+        binding.recyclerViewListGallery.adapter = adapter
     }
 
 }
--- app/src/main/java/com/dot/baseandroid/utils/AppExtension.kt
+++ app/src/main/java/com/dot/baseandroid/utils/AppExtension.kt
@@ -3,8 +3,12 @@ package com.dot.baseandroid.utils
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.GenericTransitionOptions
 import com.dot.baseandroid.BuildConfig
 import com.dot.baseandroid.data.Constants
+import com.dot.baseandroid.utils.glide.GlideApp
 
 fun logDebug(message: String) {
     if (BuildConfig.DEBUG) Log.d(Constants.TAG_DEBUG, message)
@@ -16,4 +20,12 @@ fun logError(message: String) {
 
 fun Context.showToast(message: String){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+@BindingAdapter("setImageUrl")
+fun AppCompatImageView.setImageUrl(imageUrl: String) {
+    GlideApp.with(context)
+        .load(imageUrl)
+        .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
+        .into(this)
 }

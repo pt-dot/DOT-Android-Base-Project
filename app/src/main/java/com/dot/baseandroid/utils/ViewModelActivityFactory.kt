16f2fe574e9c71c221248e8469742f37cd89f08a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-10-30 06:59:29 +0700

    :sparkles: send data detail notification

---
+++ app/src/main/java/com/dot/baseandroid/utils/ViewModelActivityFactory.kt
@@ -0,0 +1,19 @@
+package com.dot.baseandroid.utils
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dot.baseandroid.menu.notification.models.NotificationModel
+import com.dot.baseandroid.menu.notification.viewmodels.DetailNotificationViewModel
+
+class ViewModelActivityFactory(private val application: Application, private val any: Any): ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass == DetailNotificationViewModel::class.java) {
+            return DetailNotificationViewModel(application, any as NotificationModel) as T
+        }
+        return super.create(modelClass)
+    }
+
+}
